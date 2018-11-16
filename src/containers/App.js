91a9516380aefff2e5d4d962f4347a40a393be0b
hnhatcuong <hnhatcuong@HNHATCUONG>
2018-11-16 12:57:43 +0700

    webpack-config-tutorial

---
+++ src/containers/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import "../styles/index.css";
+class App extends Component {
+  render() {
+    return <div className="btn btn-primary">Hello</div>;
+  }
+}
+export default App;
