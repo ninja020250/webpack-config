ffd4a60416cf4a8dbccb5dd66fc3e3c3481b8017
Huỳnh Nhật Cường <nhatcuonghuynh@gmail.com>
2019-01-31 22:56:06 +0700

    update 2 libs, and change title

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import "../styles/index.css";
+import "../styles/index.scss";
 class App extends Component {
   render() {
-    return <div className="btn btn-primary">Hello</div>;
+    return <h1 style={{margin: "auto", textAlign: "center"}}>working hard making money</h1>;
   }
 }
 export default App;
